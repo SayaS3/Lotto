a4d1b0e948377fbce9d29a0ed77116cfa47a4af7
sayas <mrvount@gmail.com>
2024-03-09 11:35:49 +0100

    added method to NumberReceiverFacade

--- src/main/java/pl/lotto/domain/numberreciever/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/domain/numberreciever/NumberReceiverFacade.java
@@ -1,4 +1,26 @@
 package pl.lotto.domain.numberreciever;
 
+import java.util.List;
+import java.util.Set;
+
 public class NumberReceiverFacade {
+    public String inputNumbers(Set<Integer> numbersFromUser) {
+        List<Integer> filteredNumbers = filterAllNumbersInOfRange(numbersFromUser);
+        if (areAllNumbersInRange(filteredNumbers)) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
+
+    private List<Integer> filterAllNumbersInOfRange(Set<Integer> numbersFromUser) {
+        return numbersFromUser.stream()
+                .filter(number -> number >= 1)
+                .filter(number -> number <= 99)
+                .toList();
+    }
+
+    private boolean areAllNumbersInRange(List<Integer> filteredNumbers) {
+        return filteredNumbers.size() == 6;
+    }
 }
